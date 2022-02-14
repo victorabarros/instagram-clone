b93691002668eb838235d75aef2d9b649bb027fc
VictorBarros <victorbarros1130@gmail.com>
2022-02-14 00:07:31 -0300

    vector icons

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { Text, View } from "react-native"
 import fonts from "./src/theme/fonts"
 import colors from "./src/theme/color"
+import AntDesign from "react-native-vector-icons/AntDesign"
 
 
 const App = () => {
   return (
     <View style={{ alignItems: "center", flex: 1, justifyContent: "center", backgroundColor: colors.primary }}>
       <Text style={{ fontSize: fonts.size.xxlg }}>Hellou</Text>
+      <AntDesign name="stepforward" size={fonts.size.xxlg} />
     </View>
   )
 }
