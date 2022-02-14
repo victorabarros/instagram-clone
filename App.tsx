8fc6b0da72de9168c0983d66d98b129759c652ef
VictorBarros <victorbarros1130@gmail.com>
2022-02-14 00:04:22 -0300

    add themes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from 'react-native';
+import React from "react"
+import { Text, View } from "react-native"
+import fonts from "./src/theme/fonts"
+import colors from "./src/theme/color"
+
 
 const App = () => {
   return (
-    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
-      <Text>Hello</Text>
+    <View style={{ alignItems: "center", flex: 1, justifyContent: "center", backgroundColor: colors.primary }}>
+      <Text style={{ fontSize: fonts.size.xxlg }}>Hellou</Text>
     </View>
-  );
-};
+  )
+}
 
 export default App;
---
+++ src/theme/color.ts
@@ -0,0 +1,15 @@
+const colors = {
+  primary: "#405DE6",
+
+  accent: '#ED4956',
+
+  border: '#dcdcdc',
+  error: '#ED4956',
+
+  white: 'white',
+  black: 'black',
+  grey: 'grey',
+  lightgrey: 'lightgrey',
+};
+
+export default colors;
