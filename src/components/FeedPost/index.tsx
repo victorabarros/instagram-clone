bb8ed2d57d0e98fe1388afb2a64979398c4d7716
VictorBarros <victorbarros1130@gmail.com>
2022-02-17 23:30:44 -0300

    add pressable

--- src/components/FeedPost/index.tsx
+++ src/components/FeedPost/index.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Image, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Image, Pressable, Text, View } from "react-native"
 import Entypo from "react-native-vector-icons/Entypo"
 import AntDesign from "react-native-vector-icons/AntDesign"
 import Feather from "react-native-vector-icons/Feather"
@@ -34,7 +34,9 @@ export interface IUser {
 export const FeedPost = (
   { comments, createdAt, description, id, image, nofComments, nofLikes, user }: IPost
 ) => {
-  const isLiked = false
+  const nofLines = 3
+  const [showDescription, setShowDescription] = useState(false)
+  const [isLiked, setIsLiked] = useState(false)
 
   return (
     <View style={styles.container} key={`post-${id}`}>
@@ -53,10 +55,13 @@ export const FeedPost = (
       </View>
 
       {/* media */}
-      <Image
-        source={{ uri: image }}
-        style={styles.media}
-      />
+      {/* TODO handle as double tap */}
+      <Pressable onPress={() => setIsLiked(!isLiked)}>
+        <Image
+          source={{ uri: image }}
+          style={styles.media}
+        />
+      </Pressable>
 
       {/* footer */}
       <View style={styles.footerContainer}>
@@ -65,22 +70,27 @@ export const FeedPost = (
             name={isLiked ? "heart" : "hearto"}
             size={24}
             style={styles.footerIcon}
-            color={colors.black} />
+            color={isLiked ? colors.accent : colors.black}
+            onPress={() => setIsLiked(!isLiked)}
+          />
           <Ionicons
             name="chatbubble-outline"
             size={24}
             style={styles.footerIcon}
-            color={colors.black} />
+            color={colors.black}
+          />
           <Feather
             name="send"
             size={24}
             style={styles.footerIcon}
-            color={colors.black} />
+            color={colors.black}
+          />
           <Feather
             name="bookmark"
             size={24}
             style={styles.footerBookmark}
-            color={colors.black} />
+            color={colors.black}
+          />
         </View>
 
         {/* likes */}
@@ -92,8 +102,11 @@ export const FeedPost = (
         </Text>
 
         {/* description */}
-        <Text style={styles.text}>
+        <Text style={styles.text} numberOfLines={showDescription ? 0 : nofLines}>
           <Text style={styles.textBold}>{user.username}</Text>{' '} {description}
+        </Text>
+        <Text onPress={() => setShowDescription(!showDescription)}>
+          {showDescription ? "less" : "more"}
         </Text>
 
         {/* comments */}
