821ca6fe8272c063bc7e5472bdcc1f3a3da27ffd
VictorBarros <victorbarros1130@gmail.com>
2022-02-20 15:44:45 -0300

    move comment input component

---
+++ src/screens/CommentScreen/Input.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { Image, Pressable, Text, TextInput, View } from "react-native"
+import { StyleSheet } from "react-native"
+import colors from "../../theme/color"
+
+
+const CommentInput = () => {
+  const onPost = () => {
+    console.log(`
+      // TODO:
+      // disable input and button
+      // request comment to server
+      // re-render new comment
+      // enablesable input and button
+      `)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        // TODO get imagem from current user
+        source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg" }}
+        style={styles.avatar}
+      />
+      <TextInput placeholder="Add a comment..." />
+      <Pressable
+        style={{ marginLeft: "auto" }}
+        hitSlop={10}
+        onPress={onPost}
+      >
+        <Text style={{ color: colors.primary, fontWeight: "bold" }}>Post</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+export default CommentInput
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    bottom: 0,
+    position: "absolute",
+    flexDirection: "row",
+    borderWidth: .3,
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingHorizontal: 10,
+  },
+  avatar: {
+    width: 30,
+    aspectRatio: 1,
+    borderRadius: 15,
+    marginRight: 5,
+  },
+})
--- src/screens/CommentScreen/index.tsx
+++ src/screens/CommentScreen/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from "react"
-import { Button, FlatList, Image, Pressable, Text, TextInput, View, ViewabilityConfig } from "react-native"
+import React from "react"
+import { FlatList, View } from "react-native"
 import comments from "../../assets/data/comments.json"
 import { Comment } from "../../components/Comment"
-import { StyleSheet } from "react-native"
+import CommentInput from "./Input"
+
 
 const CommentScreen = () => {
   return (
@@ -12,50 +13,9 @@ const CommentScreen = () => {
         renderItem={({ item }) => <Comment {...item} showDetails />}
         showsVerticalScrollIndicator={false}
       />
-      <View style={styles.commentInputContainer}>
-        <Image
-          // TODO get imagem from current user
-          source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg" }}
-          style={styles.avatar}
-        />
-        <TextInput placeholder="Add a comment..." />
-        <Pressable
-          style={{ marginLeft: "auto" }}
-          hitSlop={10}
-          onPress={() => {
-            console.log(`
-            // TODO:
-            // disable input and button
-            // request comment to server
-            // re-render new comment
-            // enablesable input and button
-            `)
-          }}
-        >
-          <Text style={{ color: "blue", fontWeight: "bold" }}>Post</Text>
-        </Pressable>
-      </View>
+      <CommentInput />
     </View>
   )
 }
 
 export default CommentScreen
-
-export const styles = StyleSheet.create({
-  commentInputContainer: {
-    width: "100%",
-    bottom: 0,
-    position: "absolute",
-    flexDirection: "row",
-    borderWidth: .3,
-    alignItems: "center",
-    backgroundColor: "white",
-    paddingHorizontal: 10,
-  },
-  avatar: {
-    width: 30,
-    aspectRatio: 1,
-    borderRadius: 15,
-    marginRight: 5,
-  },
-})
