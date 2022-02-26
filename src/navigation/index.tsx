988740955e334f2d3d39c9e59b049ef48ec29f09
VictorBarros <victorbarros1130@gmail.com>
2022-02-26 19:14:23 -0300

    add navigation stack

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,15 +1,22 @@
 import React from "react"
-import { View } from "react-native"
-
 import { NavigationContainer } from "@react-navigation/native"
 import ProfileScreen from "../screens/ProfileScreen"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "../screens/HomeScreen"
+
+const { Navigator, Screen } = createNativeStackNavigator()
 
 export const Navigation = () => {
   return (
     <NavigationContainer>
-      {/* <HomeScreen /> */}
-      {/* <CommentScreen /> */}
-      <ProfileScreen />
+      <Navigator
+        initialRouteName="Feed"
+        screenOptions={{ headerShown: false }}
+      >
+        <Screen name="Feed" component={HomeScreen} />
+        <Screen name="Profile" component={ProfileScreen} />
+        {/* <CommentScreen /> */}
+      </Navigator>
     </NavigationContainer>
   )
 }
