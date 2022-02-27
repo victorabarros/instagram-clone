15f36669678ace76cff55809b43e875fa5f94efd
VictorBarros <victorbarros1130@gmail.com>
2022-02-27 16:30:16 -0300

    remove semmi comma

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,57 +1,57 @@
-import { RouteProp } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 
 export type RootNavigatorParamList = {
-  Home: undefined;
-  Comments: { postId: string };
-};
+  Home: undefined
+  Comments: { postId: string }
+}
 
 export type BottomTabNavigatorParamList = {
-  HomeStack: undefined;
-  Search: undefined;
-  Upload: undefined;
-  Notifications: undefined;
-  MyProfile: undefined;
-};
+  HomeStack: undefined
+  Search: undefined
+  Upload: undefined
+  Notifications: undefined
+  MyProfile: undefined
+}
 
 export type MyProfileNavigationProp = BottomTabNavigationProp<
   BottomTabNavigatorParamList,
   'MyProfile'
->;
+>
 
 export type MyProfileRouteProp = RouteProp<
   BottomTabNavigatorParamList,
   'MyProfile'
->;
+>
 
 export type HomeStackNavigatorParamList = {
-  Feed: undefined;
-  Comments: { postId: string };
-  Profile: { userId: string };
-};
+  Feed: undefined
+  Comments: { postId: string }
+  Profile: { userId: string }
+}
 
 export type UserProfileNavigationProp = NativeStackNavigationProp<
   HomeStackNavigatorParamList,
   'Profile'
->;
+>
 
 export type UserProfileRouteProp = RouteProp<
   HomeStackNavigatorParamList,
   'Profile'
->;
+>
 
 export type FeedNavigationProp = NativeStackNavigationProp<
   HomeStackNavigatorParamList,
   'Feed'
->;
+>
 
 export type ProfileStackNavigatorParamList = {
-  Profile: undefined;
-  'EditProfile': undefined;
-};
+  Profile: undefined
+  'EditProfile': undefined
+}
 
 export type ProfileNavigationProp = NativeStackNavigationProp<
   ProfileStackNavigatorParamList,
   'Profile'
->;
+>
