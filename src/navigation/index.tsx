8db8e855c492f5f41ff49cf2a3958eb0bb07f042
VictorBarros <victorbarros1130@gmail.com>
2022-02-27 16:46:51 -0300

    deep linking

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,17 +1,38 @@
 import React from "react"
-import { NavigationContainer } from "@react-navigation/native"
+import { LinkingOptions, NavigationContainer } from "@react-navigation/native"
 import { ProfileScreen } from "../screens/ProfileScreen"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import HomeScreen from "../screens/HomeScreen"
 import { BottomTab } from "./BottomTab"
 import CommentScreen from "../screens/CommentScreen"
 import { RootNavigatorParamList } from "./types"
+import { Text } from "react-native"
 
 const { Navigator, Screen } = createNativeStackNavigator<RootNavigatorParamList>()
 
+const linking: LinkingOptions<RootNavigatorParamList> = {
+  prefixes: ['<https://notjustinsta.com>', 'notjustinsta://'],
+  config: {
+    initialRouteName: 'Home',
+    screens: {
+      Comments: 'comments',
+      Home: {
+        screens: {
+          HomeStack: {
+            initialRouteName: 'Feed', // <- to be able to go back to the feed
+            screens: {
+              UserProfile: 'user/:userId'
+            }
+          }
+        }
+      }
+    },
+  },
+}
+
 export const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Navigator screenOptions={{ headerShown: false }}>
 
         <Screen name="Home" component={BottomTab} />
