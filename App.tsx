40fcfd5f30e7dd4fd4d37e59702d3c1a1b0fac03
VictorBarros <victorbarros1130@gmail.com>
2022-03-09 21:53:58 -0300

    basic authentication configs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,60 @@
 import React from "react"
 import { Navigation } from "./src/navigation"
-import Amplify from "aws-amplify"
+import Amplify, { Auth } from "aws-amplify"
+import { withAuthenticator, AmplifyTheme } from "aws-amplify-react-native"
 import config from "./src/aws-exports"
 
 Amplify.configure(config)
+// Auth.signOut()
 
 const App = () => <Navigation />
 
-export default App
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Username",
+      key: "username",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+      placeholder: "Username",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+      placeholder: "Email",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 3,
+      type: "string",
+      placeholder: "Password",
+    }
+  ]
+}
+
+const customTheme = {
+  ...AmplifyTheme,
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: "blue",
+    borderRadius: 100,
+  },
+  buttonDisabled: {
+    ...AmplifyTheme.buttonDisabled,
+    backgroundColor: "lightblue",
+    borderRadius: 100,
+  },
+  sectionFooterLink: {
+    ...AmplifyTheme.sectionFooterLink,
+    color: "blue",
+  },
+}
+
+export default withAuthenticator(App, { signUpConfig, theme: customTheme })
