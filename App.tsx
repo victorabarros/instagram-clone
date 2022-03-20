50ab5cfcdb0eb80b3611f9ad56e6b74ebfc157dd
VictorBarros <victorbarros1130@gmail.com>
2022-03-20 17:59:57 -0300

    use custom auth pages

--- App.tsx
+++ App.tsx
@@ -1,60 +1,9 @@
 import React from "react"
 import { Navigation } from "./src/navigation"
-import Amplify, { Auth } from "aws-amplify"
-import { withAuthenticator, AmplifyTheme } from "aws-amplify-react-native"
+import Amplify from "aws-amplify"
 import config from "./src/aws-exports"
 
 Amplify.configure(config)
 // Auth.signOut()
 
-const App = () => <Navigation />
-
-const signUpConfig = {
-  hideAllDefaults: true,
-  signUpFields: [
-    {
-      label: "Username",
-      key: "username",
-      required: true,
-      displayOrder: 1,
-      type: "string",
-      placeholder: "Username",
-    },
-    {
-      label: "Email",
-      key: "email",
-      required: true,
-      displayOrder: 2,
-      type: "string",
-      placeholder: "Email",
-    },
-    {
-      label: "Password",
-      key: "password",
-      required: true,
-      displayOrder: 3,
-      type: "string",
-      placeholder: "Password",
-    }
-  ]
-}
-
-const customTheme = {
-  ...AmplifyTheme,
-  button: {
-    ...AmplifyTheme.button,
-    backgroundColor: "blue",
-    borderRadius: 100,
-  },
-  buttonDisabled: {
-    ...AmplifyTheme.buttonDisabled,
-    backgroundColor: "lightblue",
-    borderRadius: 100,
-  },
-  sectionFooterLink: {
-    ...AmplifyTheme.sectionFooterLink,
-    color: "blue",
-  },
-}
-
-export default withAuthenticator(App, { signUpConfig, theme: customTheme })
+export const App = () => <Navigation />
